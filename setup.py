b1ea768d76fe60da0c8e554185fc6659315aade0
Antonin Delpeuch <antonin@delpeuch.eu>
2020-11-24 17:26:06 +0100

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1',
+    version='0.2.0',
 
     description='A parser for wikipedia citation templates',
     long_description=long_description,
@@ -50,9 +50,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
     ],
 
     # What does your project relate to?
